9e0d002b8e1a05728820a3e88123b98aa2d241e9
mockybellasio <43879122+mockybellasio@users.noreply.github.com>
2018-12-12 17:30:31 +0100

    cv

--- src/app/form-inscription/form-inscription.service.ts
+++ src/app/form-inscription/form-inscription.service.ts
@@ -1,6 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { FormUser } from '../ModelFolder/FormUser';
 
 const URL_BACKEND = environment.baseUrl
