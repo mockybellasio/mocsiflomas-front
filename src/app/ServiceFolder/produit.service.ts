06ab1d5bc928c9bf897e4ce07a20615cab51725d
Simon-Benezet <simon.benezet@hotmail.fr>
2018-12-17 14:59:46 +0100

    a

--- src/app/ServiceFolder/produit.service.ts
+++ src/app/ServiceFolder/produit.service.ts
@@ -15,27 +15,20 @@ export class ProduitService {
 
   //version Promise
   listerProduits(): Promise<Produit[]> {
-    return this._http.get(URL_BACKEND + 'produits').toPromise()
+    return this._http.get(URL_BACKEND + 'produits/liste-produits').toPromise()
       .then((tabProd: any[]) => tabProd.map(prod => new Produit(prod.nomSaga, prod.nomImage, prod.personnage, prod.nomFigurine, prod.taille, prod.prix, prod.description, prod.numeroFigurine)))
   }
 
   //chercher par nomFigurine
   chercherParNom(nomFigurine: String): Observable<Produit> {
-    return this._http.get<Produit>(URL_BACKEND + `${nomFigurine}`)
-
+    return this._http.get<Produit>(URL_BACKEND + `produit/${nomFigurine}`)
   }
 
   //base posée A MODIFIER
   modifierProduit(unProduit: Produit, produit: Produit): Promise<Produit> {
-    return this._http.patch(`${URL_BACKEND}gestion-produits/${unProduit.nomFigurine}`,
+    return this._http.patch(`${URL_BACKEND}gestion-produits/modifier-produit/${unProduit.nomFigurine}`,
       produit, { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) })
       .toPromise().then((p: Produit) => p)
   }
 
-  //
-  ajouterProduit(prod: AjoutProduit): Observable<any> {
-    return this._http.post(`${URL_BACKEND}produit/creer`, prod);
-    //.subscribe((c: AjoutProduit) => c = new AjoutProduit())
-    // .then((c: AjoutProduit) => c = new AjoutProduit())
-  }
 }
