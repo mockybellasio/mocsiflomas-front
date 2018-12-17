da19cf8d67061790e7dceef0d841485e75d778f4
Champetier <todave11@gmail.com>
2018-12-17 18:46:01 +0100

    Go

--- src/app/panier/panier.service.ts
+++ src/app/panier/panier.service.ts
@@ -9,11 +9,16 @@ export class PanierService {
   constructor() { }
 
   addPanier(qte:number ,pops: Produit): void {
-    let panier: Map<Produit,number>;
-    if (localStorage.getItem('panier') !== undefined) {
-      panier = JSON.parse(localStorage.getItem('panier'));
+    let panier: Map<Produit,number>=new Map();
+    let retrievedObject = localStorage.getItem('panier');
+    if (retrievedObject !== undefined) {
+      panier = new Map(JSON.parse(retrievedObject).map(i=>[i[0],i[1]]));
+      console.log(panier.has(pops))
+
     }
     panier.set(pops,qte);
-    localStorage.setItem('testObject', JSON.stringify(panier));
+    let arrayPanier=Array.from((panier))
+    localStorage.setItem('panier', JSON.stringify(arrayPanier));
+    console.log(new Map(JSON.parse(retrievedObject).map(i=>[i[0],i[1]])));
   }
 }
