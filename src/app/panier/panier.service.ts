eb5c19c8a2eea2042631f476297c740622efe980
Champetier <todave11@gmail.com>
2018-12-18 16:02:45 +0100

    pagner

--- src/app/panier/panier.service.ts
+++ src/app/panier/panier.service.ts
@@ -8,49 +8,49 @@ export class PanierService {
 
   constructor() { }
 
-  addPanier(qte:number ,pops: Produit): void {
-    let panier: Map<Produit,number>=new Map();
-    let retrievedObject = localStorage.getItem('panier');
-    if (retrievedObject !== null) {
-      panier = new Map(JSON.parse(retrievedObject).map(i=>[i[0],i[1]]));
-      console.log(panier.has(pops))
+  addPanier(qte: number, pops: Produit): void {
+    //variable pour verifier si l'on fait une maj du pagné
+    let upadate: Boolean = false;
+    //panier version map
+    let mapPanier: Map<Produit, number> =new Map();
+    //panier aprés la transformation en array
+    let panierArray
+    //etat actuel du panier
+    let panier = localStorage.getItem('panier');
+    //Si le panier contien un element
+    if (panier !== null) {
+      panierArray = JSON.parse(panier)
+      mapPanier.set(pops, qte);
+      let elementArray = Array.from(mapPanier);
+      panierArray.forEach(element => {
+        //si le panier contien dejat un élément du même nom alors met a jour élément 
+        if (element[0].nomFigurine == elementArray[0][0].nomFigurine) {
+          element[1] = elementArray[0][1];
+          upadate = true
+        }
+      });
+      //passer array en map pour ajouter nouvel element ou mettre a jour array
+      mapPanier = new Map(panierArray);
+      if (!upadate) {
+        mapPanier.set(pops, qte);
+      }
 
+      let arrayPanier = Array.from((mapPanier))
+      localStorage.setItem('panier', JSON.stringify(arrayPanier));
+    } else {
+      mapPanier.set(pops, qte);
+      //console.log(pops,qte);
+      let arrayPanier = Array.from((mapPanier))
+      localStorage.setItem('panier', JSON.stringify(arrayPanier));
     }
-    panier.set(pops,qte);
-    let arrayPanier=Array.from((panier))
-    localStorage.setItem('panier', JSON.stringify(arrayPanier));
-    console.log(new Map(JSON.parse(retrievedObject).map(i=>[i[0],i[1]])));
   }
 
-      // let a=new Produit("","","","",5,3,"",5)
-    // panier.set(a,5)
-   /* addPanier(qte: number, pops: Produit): void {
-
-      let mapPanier: Map<Produit, number> = new Map();
-      let panierArray
-      let panier = localStorage.getItem('panier');
-      if (panier !== null) {
-        // panier = new Map(
-        //   JSON.parse(retrievedObject)
-        //   .map(i=>[i[0],i[1]]));
-        panierArray = JSON.parse(panier)
-        mapPanier.set(pops, qte);
-        console.log(Array.from(mapPanier)[0])
-        panierArray.forEach(element => {  
-          //console.log(element[0])
-          //console.log(element[0] === Array.from(mapPanier)[0]);
-        });
-        mapPanier =new Map(panierArray);
-        mapPanier.set(pops, qte);
-        let arrayPanier = Array.from((mapPanier))
-        localStorage.setItem('panier', JSON.stringify(arrayPanier));
-        //passer array en map pour ajouter nouvel element ou mettre a jour array
-  
-      }
-  
-  
-      //let arrayPanier = Array.from((panierArray))
-      //localStorage.setItem('panier', JSON.stringify(arrayPanier));
-      //console.log(new Map(JSON.parse(retrievedObject).map(i=>[i[0],i[1]])));
-    }*/
+  readPanier():Array<any>{
+    let panier = localStorage.getItem('panier');
+    if (panier !== null) {
+      return JSON.parse(panier);
+    }else{
+      return null;
+    } 
+  }
 }
